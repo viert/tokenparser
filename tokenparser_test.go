c78779bb597083a7c67b373b5f40079692b454fd
Pavel Vorobyov <aquavitale@yandex.ru>
2014-02-02 16:07:28 +0400

    benchmark added

--- tokenparser_test.go
+++ tokenparser_test.go
@@ -89,3 +89,40 @@ func TestParseStrict(t *testing.T) {
 	checkField(t, "cookies", cookies, "_session=3829834;")
 	checkField(t, "reqtime", reqtime, "28.314")
 }
+
+func BenchmarkStrictParse(b *testing.B) {
+	testString := `[28/Jun/2013 12:54:48] example.com "GET /some/url HTTP/1.0" "Mozilla" "_session=3829834;" 28.314` + "\n"
+	parser := New()
+	var date, tm, vhost, method, url, browser, cookies, reqtime string
+
+	parser.Skip('[')
+	parser.UpTo(' ', &date)
+	parser.Skip(' ')
+	parser.UpTo(']', &tm)
+	parser.SkipAny()
+	parser.SkipAny()
+	parser.UpTo(' ', &vhost)
+	parser.Skip(' ')
+	parser.SkipAny()
+	parser.UpTo(' ', &method)
+	parser.Skip(' ')
+	parser.UpTo(' ', &url)
+	parser.Skip(' ')
+	parser.SkipTo(' ')
+	parser.Skip(' ')
+	parser.Skip('"')
+	parser.UpTo('"', &browser)
+	parser.Skip('"')
+	parser.SkipTo('"')
+	parser.Skip('"')
+	parser.UpTo('"', &cookies)
+	parser.Skip('"')
+	parser.Skip(' ')
+	parser.UpTo('\n', &reqtime)
+	parser.Skip('\n')
+	parser.Strict = true
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		parser.ParseString(testString)
+	}
+}
